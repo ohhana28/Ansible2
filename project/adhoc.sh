0fcc70bdd13c8834a09857e067650da5c032b429
ohhana28 <140497546+ohhana28@users.noreply.github.com>
2023-09-21 10:50:48 +0900

    Delete project directory

--- project/adhoc.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-ansible all -m yum_repository \
--a "name=MyBase description='base software' baseurl='http://192.168.10.10/pkg/BaseOS' gpgcheck=no"
-
-ansible all -m yum_repository \
--a "name=MyAppStream description='Appstream software' baseurl='httpd://192.168.10.10/pkg/AppStream/' gpgcheck=no"
